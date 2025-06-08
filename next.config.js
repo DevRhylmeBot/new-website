d337584431b465ec67985badf782cbf9458a38f1
Durgesh kumar prajapati <98798977+Durgesh4993@users.noreply.github.com>
2025-06-08 19:01:10 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint{
+  ignoreDuringBuilds:true;
+},
   images: {
     domains: ['i.ibb.co'], // Add i.ibb.co to allow image loading from this domain
   },
