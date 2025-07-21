0530e7f224e8af9d7cfe89d9bd6d1ff32ad6eb4f
DevRhylmeBot <devrhylme1@gmail.com>
2025-07-22 03:17:04 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: {
-    domains: ['i.ibb.co'],
-  },
+  // Add any other config options here
 };
 
 module.exports = nextConfig;
